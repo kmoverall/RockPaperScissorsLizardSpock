990327cb707df087fdda709bd333e1ab78c07d80
Graham Tucker <gttu222@uky.edu>
2013-11-07 19:54:51 -0500

    joinGame

--- src/com/example/rockpaperscissorslizardspock/MainActivity.java
+++ src/com/example/rockpaperscissorslizardspock/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.rockpaperscissorslizardspock;
 
-import android.os.Bundle;
+import android.R.string;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+	
+	
+
+	
+	
 	}
 
 	@Override
@@ -18,5 +31,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 }
