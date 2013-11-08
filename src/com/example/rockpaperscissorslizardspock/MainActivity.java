f6fddda7d198282ca13e9f43521a340c6e21259e
kmoverall <kmoverall@gmail.com>
2013-11-07 19:55:03 -0500

    Completed step 6

--- src/com/example/rockpaperscissorslizardspock/MainActivity.java
+++ src/com/example/rockpaperscissorslizardspock/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.rockpaperscissorslizardspock;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+
+	    Button joinButton = (Button)findViewById(R.id.button2);
+	    
+	    joinButton.setOnClickListener(new View.OnClickListener() {
+
+	      @Override
+	      public void onClick(View view) {
+	        Intent intent = new Intent(MainActivity.this, JoinGame.class);
+	        
+	        String name = ((EditText)findViewById(R.id.editText1)).getText().toString();
+	        
+	        intent.putExtra("name", name);
+	        startActivity(intent);
+	      }
+
+	    });
 	}
 
 	@Override
